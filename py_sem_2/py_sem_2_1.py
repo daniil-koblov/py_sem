5e02ec8c16ee2cacf2b9889845d8d0bf028f6eac
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-02 15:16:17 +0300

    third add py_sem_2_1 and py_sem_2_2

--- py_sem_2/py_sem_2_1.py
+++ py_sem_2/py_sem_2_1.py
@@ -1,11 +1,11 @@
 num = int(input('Введите число: '))
-fakt = 1
+fakt = 1                            # Первый вариант.
 while num > 1:
     fakt *= num
     num -= 1
 print(f'Факториал равен {fakt}.')
 
-fact = 1
+fact = 1                           # Второй вариант.
 for i in range(1, num + 1):
     fact *= i 
 print(f'Факториал равен {fact}.')
