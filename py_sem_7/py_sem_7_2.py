2cf0ca288321d7b84af17d823fcf767d5c2a9aa9
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-02-16 20:58:49 +0300

    first add py_sem_7_2.py

---
+++ py_sem_7/py_sem_7_2.py
@@ -0,0 +1,18 @@
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# print(*find_farthest_orbit(orbits))
+
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+
+def find_farthest_orbit(orbits):
+    pi = 3.14
+    orbits_new = [(0, 0)]
+    max = 0
+    for i in orbits:
+        if i[0] != i[1]:
+            if max < pi * i[0] * i[1]:
+                max = pi * i[0] * i[1]
+                orbits_new.pop()
+                orbits_new.append(i)
+    return orbits_new
+
+print(*find_farthest_orbit(orbits))
